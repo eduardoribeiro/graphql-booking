f7f550955579f176d05315b4d4214e80e962bde9
Raul <raulch1998@gmail.com>
2019-02-18 09:48:42 -0400

    Booking List Component Added

---
+++ client/src/components/Bookings/BookingList/BookingList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './BookingList.css';
+
+const bookingList = props => (
+  <ul className="bookings-list">
+    {props.bookings.map(booking => (
+      <li key={booking._id} className="bookings-item">
+        <div className="bookings-item-data">
+          {booking.event.title} -{' '}
+          {new Date(booking.createdAt).toLocaleDateString()}
+        </div>
+        <div className="bookings-item-actions">
+          <button
+            className="btn btn-red"
+            onClick={props.onDelete.bind(this, booking._id)}
+          >
+            Cancel
+          </button>
+        </div>
+      </li>
+    ))}
+  </ul>
+);
+
+export default bookingList;
